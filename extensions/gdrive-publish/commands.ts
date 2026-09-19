411d3ce756072b0861a5afa706d060adb49c7d00
Stefan Marx <stefan@marx-merten.de>
2026-09-19 15:49:55 +0200

    ship compiled js in the npm package, fixing installed runs

--- extensions/gdrive-publish/commands.ts
+++ extensions/gdrive-publish/commands.ts
@@ -12,7 +12,7 @@
  */
 
 /** Tool version, independent of the monorepo's package version. */
-export const VERSION = "0.2.0";
+export const VERSION = "0.2.1";
 
 export const BIN = "gdrive-publish";
 
